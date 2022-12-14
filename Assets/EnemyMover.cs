d41d7589122c767f6adf783a860d63e96e8c8f45
Andrew Dinspechin <58988010+akvaxthelich@users.noreply.github.com>
2022-12-14 03:12:42 -0500

    sleepy time for me :)

--- Assets/EnemyMover.cs
+++ Assets/EnemyMover.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     public float speed = 3;
+    public GameObject player;
 
     void Start()
     {
         Destroy(this.gameObject, 15);
+        player = GameObject.Find("Player");
     }
 
 
     void Update()
     {
+        transform.LookAt(player.transform.position);
         transform.Translate(-transform.right * speed * Time.deltaTime);
     }
 }
